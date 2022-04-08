304a43bfdf9ac07ce528d46cf8e57a4ccdfaf904
Justin Schreiber <jschreiber234@gmail.com>
2022-04-08 14:39:29 -0400

    added project cards

--- src/Projects.js
+++ src/Projects.js
@@ -1,18 +1,28 @@
 import Project from "./projects.json"
+import Card from 'react-bootstrap/Card'
+import { Container } from "react-bootstrap";
 const Projects = () => {
-    return ( 
+    return (
         <div>
-            <p className="text-center">PLACEHOLDER TEXT</p>
+            <Container>
+            {RenderProjects}
+            </Container>           
         </div>
-     );
+    );
 }
 
-function RenderProjects(){
-    return ( 
-        <div>
-            <p className="text-center">{Project.projects[0]["name"]}</p>
-        </div>
-     );
-}
- 
+const RenderProjects = Project.projects.map(projectItem =>
+    <div key={projectItem.name} className="d-flex justify-content-center">        
+        <Card >
+            <Card.Body>
+                <Card.Title><a href={projectItem.link}>{projectItem.name}</a></Card.Title>
+                <Card.Text>
+                {projectItem.description}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </div>
+);
+
 export default Projects;
+

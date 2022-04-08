715037d15b317514f3af0fcfc7bf0fbb02a90c74
Justin Schreiber <jschreiber234@gmail.com>
2022-04-08 16:50:25 -0400

    added bio

--- src/About.js
+++ src/About.js
@@ -1,11 +1,24 @@
+import Container from 'react-bootstrap/Container'
 const About = () => {
-    return ( 
+    return (
         <div>
-            <p className="text-center">
-                PLACEHOLDER TEXT
-            </p>
+            <Container>
+                <h3 className='pt-2'>
+                    Justin Schreiber
+                </h3>
+                <Container className='d-flex justify-content-start pt-3'>
+                    <img
+                        width="247"
+                        height="247"
+                        src="/img/profile.jpeg"
+                        alt="profile"
+                    />
+                    <p className='ps-3 pt-5 text-end'>Recent Graduate of Computer Science at Queens College &nbsp; Proficient in Javascript, Java, NodeJs, React and more</p>
+                </Container>
+
+            </Container>
         </div>
-     );
+    );
 }
- 
+
 export default About;

b08277d3a5f71be0e9b8af6418c5a2d848d96e67
Justin Schreiber <jschreiber234@gmail.com>
2022-04-10 15:45:26 -0400

    updates to profile image layout

--- src/About.js
+++ src/About.js
@@ -3,19 +3,19 @@ const About = () => {
     return (
         <div>
             <Container>
-                <h3 className='pt-2'>
-                    Justin Schreiber
-                </h3>
-                <Container className='d-flex justify-content-start pt-3'>
+                <Container className='row pt-3 w-75 mx-auto'>
                     <img
-                        width="247"
-                        height="247"
+                        className='col'
                         src="/img/profile.jpeg"
                         alt="profile"
                     />
-                    <p className='ps-3 pt-5 text-end'>Recent Graduate of Computer Science at Queens College &nbsp; Proficient in Javascript, Java, NodeJs, React and more</p>
+                    <div className='col'>
+                        <h3 className='ps-3 text-center'>
+                            Justin Schreiber
+                        </h3>
+                        <p className='ps-3 pt-2 text-center'>Recent Graduate of Computer Science at Queens College. Proficient in Javascript, Java, NodeJs, React and more.</p>
+                    </div>
                 </Container>
-
             </Container>
         </div>
     );

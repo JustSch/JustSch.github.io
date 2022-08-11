b39739d639e835a72ddc535bc106fb7d47fd4e9f
Justin Schreiber <jschreiber234@gmail.com>
2022-08-11 02:53:26 -0400

    more changes

--- src/NavbarElement.js
+++ src/NavbarElement.js
@@ -1,6 +1,11 @@
 import { Container, Nav, Button } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 //@TODO fix active highlight
+
+
+
+
+
 const NavbarElement = () => {
     return ( 
         <Navbar bg="light" variant="light" expand="lg">

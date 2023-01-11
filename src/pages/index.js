261af8831a3a1e89740d595ca41fc43e0e2519cf
Justin Schreiber <jschreiber234@gmail.com>
2023-01-10 22:20:47 -0500

    add meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
-import Home from './home'
+import Home from './home';
+import Head from 'next/head';
 
 function App() {
   return (
-    <Home />
+    <div>
+      <Head>
+        <title>Justin Schreiber's Portfolio</title>
+        <meta property="og:title" content="Justin Schreiber's Portfolio" />
+        <meta property="og:description" content="An Aspiring Software Engineer" />
+        <meta property="og:url" content="https://JustSch.github.io" />
+      </Head>
+      <Home />
+    </div>
+
   );
 }
 

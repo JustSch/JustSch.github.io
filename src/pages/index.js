96d52eca3ce3ed5b39798b5b070c409bf3879e16
Justin Schreiber <jschreiber234@gmail.com>
2023-03-09 00:55:58 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ function App() {
       <Head>
         <title>Justin Schreiber's Portfolio</title>
         <meta property="og:title" content="Justin Schreiber's Portfolio" />
-        <meta property="og:description" content="An Aspiring Software Engineer" />
+        <meta property="og:description" content="Software Engineer" />
         <meta property="og:url" content="https://JustSch.github.io" />
         <meta property="og:image" content="/img/profile.jpeg" />
       </Head>
